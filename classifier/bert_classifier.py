50d279e789689d83faca5241e441893c1a37682b
Sahit Dendekuri <sahitdendekuri@DN0a1f6576.SUNet>
2023-02-23 00:28:40 -0800

    Updated classifier

--- classifier/bert_classifier.py
+++ classifier/bert_classifier.py
@@ -6,10 +6,16 @@ from transformers import BertTokenizer, BertForSequenceClassification, AdamW
 # Load the pre-trained BERT model and tokenizer
 model_name = 'bert-base-uncased'
 tokenizer = BertTokenizer.from_pretrained(model_name)
+print("after tokenizer")
 model = BertForSequenceClassification.from_pretrained(model_name, num_labels=5)
+print("after model")
+
+
 
 # Load the labeled song lyric CSV file
 data = pd.read_csv('data/clean-data.csv')
+data['Genre'] = data['Genre'].astype('category')
+print(data['Lyric'].dtype)
 
 # Tokenize the lyrics using the BERT tokenizer
 encoded_data = tokenizer.batch_encode_plus(
@@ -25,15 +31,22 @@ encoded_data = tokenizer.batch_encode_plus(
 # Convert the tokenized lyrics to a BERT-compatible format
 input_ids = encoded_data['input_ids']
 attention_masks = encoded_data['attention_mask']
-labels = torch.tensor(data.Genre0.values)
+labels = torch.tensor(data.Genre.cat.codes.values)
+
+batch_size, seq_length = input_ids.size()
+num_batches = batch_size // 512
+input_ids = input_ids.narrow(0, 0, num_batches * 512).view(-1, 512)
+attention_masks = attention_masks.narrow(0, 0, num_batches * 512).view(-1, 512)
+
+
 
 # Split the data into training and validation sets
-train_inputs, validation_inputs, train_labels, validation_labels = train_test_split(input_ids, labels,
+num_samples = min(len(input_ids), len(labels))
+train_inputs, validation_inputs, train_labels, validation_labels = train_test_split(input_ids[:num_samples], labels[:num_samples],
                                                                                     random_state=2022,
                                                                                     test_size=0.1)
-train_masks, validation_masks, _, _ = train_test_split(attention_masks, input_ids,
+train_masks, validation_masks, _, _ = train_test_split(attention_masks[:num_samples], input_ids[:num_samples],
                                                        random_state=2022, test_size=0.1)
-
 # Define the fine-tuning model architecture
 model = BertForSequenceClassification.from_pretrained(
     model_name,
@@ -49,6 +62,7 @@ epochs = 4
 
 # Fine-tune the model on the training set
 for epoch in range(epochs):
+    print("in epoch", epoch)
     model.train()
     train_loss = 0
     for batch in range(len(train_inputs)):
